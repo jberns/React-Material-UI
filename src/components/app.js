f2acb1c7a74fed1b123a2762b60b43058534ccf2
Josh Bernstein <school.josh@gmail.com>
2018-04-26 20:46:14 -0600

    Added Button and Menu Testing

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import ButtonTest from './component_button';
+import SimpleMenu from './component_menu';
+import MenuListComposition from './menu-popup';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <div>React simple starter</div>
+
+        <div><ButtonTest /></div>
+
+        <div><SimpleMenu /></div>
+
+        <div><MenuListComposition /></div>
+
+
+      </div>
+
     );
   }
 }
---
+++ src/components/component_button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'material-ui/Button';
+
+const ButtonTest = () => {
+
+  return(
+    <Button variant = "raised" color="primary">
+      Hello World
+    </Button>
+  );
+
+};
+
+export default ButtonTest;
